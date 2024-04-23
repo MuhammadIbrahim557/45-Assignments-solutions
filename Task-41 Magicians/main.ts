190070c25d6684104c115ccf79562fb04acd46e1
Muhammad Ibraheem <160319187+MuhammadIbrahim557@users.noreply.github.com>
2024-04-23 12:32:26 -0700

    Solved it in even shorter method and commented bigger method

--- Task-41 Magicians/main.ts
+++ Task-41 Magicians/main.ts
@@ -1,7 +1,17 @@
-let magicians:string[] = ["Lalu", "khan", "Xavier", "jojo"]
-function show_magiciacns(magicians:string[]){
-    magicians.forEach(magicians =>{
-        console.log(magicians)
-    })
+let magiciansNames:string [] = ["Jojo", "loppy", "Puppy"] 
+function show_magicians (){
+    console.log(magiciansNames)
 }
-console.log(magicians);
+show_magicians()
+
+
+// Another way to do it:
+// let magicians:string[] = ["Lalu", "khan", "Xavier", "jojo"]
+// function show_magiciacns(magicians:string[]){
+//     magicians.forEach(magicians =>{
+//         console.log(magicians)
+//     })
+// }
+// console.log(magicians);
+
+
