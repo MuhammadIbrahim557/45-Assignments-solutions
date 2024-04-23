597bd9e7dc3b439a288092f7859fd7ed3683a821
Muhammad Ibraheem <160319187+MuhammadIbrahim557@users.noreply.github.com>
2024-04-23 12:30:43 -0700

    Solved it in even shorter method

--- Task-42 Great Magicians/main.ts
+++ Task-42 Great Magicians/main.ts
@@ -1,18 +1,16 @@
-
+// exercise no 41
 let magicians: string[] = ["Lalu", "khan", "Xavier", "jojo"];
 
-function show_magicians(magicians: string[]) {
-    magicians.forEach(magician => {
-        console.log(magician);
-    });
+function show_magicians() {
+  console.log(magicians);
 }
 
-function make_great(magicians: string[]){
-    for(let i = 0; i < magicians.length; i++){
-        magicians[i] = "The great " + magicians[i];
-    }
+// exercise no 42
+function make_great(magicians: string[]) {
+  for (let i = 0; i < magicians.length; i++) {
+    magicians[i] = "The great " + magicians[i];
+  }
 }
-
 
 console.log("Original list of magicians:");
 console.log(magicians);
@@ -21,5 +19,4 @@ make_great(magicians);
 
 console.log("Modified list of magicians:");
 
-show_magicians(magicians);
-
+show_magicians();
